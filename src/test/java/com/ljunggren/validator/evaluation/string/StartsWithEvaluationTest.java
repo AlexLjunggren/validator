b51d40aeb6712c756ef6efd7a9ab8b4c6c4b9239
aljunggren <aljunggren@allegient.com>
2021-05-18 22:28:01 -0400

    Removed unused import

--- src/test/java/com/ljunggren/validator/evaluation/string/StartsWithEvaluationTest.java
+++ src/test/java/com/ljunggren/validator/evaluation/string/StartsWithEvaluationTest.java
@@ -1,10 +1,10 @@
 package com.ljunggren.validator.evaluation.string;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-
-import com.ljunggren.validator.evaluation.string.StartsWithEvaluation;
 
 public class StartsWithEvaluationTest {
 
